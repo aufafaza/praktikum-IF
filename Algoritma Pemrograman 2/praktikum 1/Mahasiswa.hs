eb05634e7743d5ea5aad8d03042c5ff31d859b10
aufafaza <aufa.rienaldifaza.a@gmail.com>
2025-09-17 06:19:09 +0700

    added praktikum 1

---
+++ Algoritma Pemrograman 2/praktikum 1/Mahasiswa.hs
@@ -0,0 +1,69 @@
+module Mahasiswa where
+
+-- TYPE MAHASISWA
+-- DEFINISI TYPE
+-- type mahasiswa: <nama: String, nim: String, ipk: Float>
+-- {<nama, nim, ipk> adalah sebuah mahasiswa, dengan nama adalah nama mahasiswa,
+--  nim adalah nomor induk mahasiswa, ipk adalah nilai IPK mahasiswa}
+
+data Mahasiswa = Mahasiswa String String Float
+  deriving (Show, Eq)
+
+-- DEFINISI DAN SPESIFIKASI SELEKTOR
+-- nama: mahasiswa -> String
+-- {nama(M) memberikan nama Mahasiswa M}
+nama :: Mahasiswa -> String
+nama (Mahasiswa name _ _) = name
+
+-- nim: mahasiswa -> String
+-- {nim(M) memberikan NIM Mahasiswa M}
+nim :: Mahasiswa -> String
+nim (Mahasiswa _ att _) = att
+
+-- ipk: mahasiswa -> Float
+-- {ipk(M) memberikan IPK Mahasiswa M}
+ipk :: Mahasiswa -> Float
+ipk (Mahasiswa _ _ score) = score
+
+-- DEFINISI DAN SPESIFIKASI KONSTRUKTOR
+-- makeMahasiswa: String -> String -> Float -> mahasiswa
+-- {makeMahasiswa(nama,nim,ipk) membentuk Mahasiswa baru}
+makeMahasiswa :: String -> String -> Float -> Mahasiswa
+makeMahasiswa = Mahasiswa 
+
+-- DEFINISI DAN SPESIFIKASI PREDIKAT
+-- isValidMahasiswa: mahasiswa -> boolean
+-- {isValidMahasiswa(M) benar jika M memiliki Nama, NIM tidak kosong dan IPK antara 0.0 s.d. 4.0}
+isValidMahasiswa :: Mahasiswa -> Bool
+isValidMahasiswa (Mahasiswa name att score) = not(null name) && not(null att) && score >= 0.0 && score <= 4.0
+
+-- DEFINISI OPERATOR/FUNGSI LAIN TERHADAP MAHASISWA
+-- gantiIPK: mahasiswa -> Float -> mahasiswa
+-- {gantiIPK(M,ipkBaru) mengganti IPK mahasiswa M dengan ipkBaru (maksimal 4.0)}
+gantiIPK :: Mahasiswa -> Float -> Mahasiswa
+gantiIPK (Mahasiswa name att score) ipkBaru 
+    | ipkBaru > 4.0 = makeMahasiswa name att 4.0
+    | ipkBaru < 0.0 = makeMahasiswa name att 0.0
+    | otherwise = makeMahasiswa name att ipkBaru
+-- klasifikasiMahasiswa: mahasiswa -> String
+-- {klasifikasiMahasiswa(M) memberikan predikat mahasiswa berdasarkan IPK}
+-- Dengan ketentuan:
+-- IPK >= 3.51 : "Cumlaude"
+-- 3.00 <= IPK < 3.51 : "Sangat Memuaskan"
+-- 2.75 <= IPK < 3.00 : "Memuaskan"
+-- IPK < 2.75 : "Perlu Perbaikan"
+klasifikasiMahasiswa :: Mahasiswa -> String
+klasifikasiMahasiswa (Mahasiswa _ _ score)
+    | score >= 3.51 && score <= 4.00 = "Cumlaude" 
+    | score >= 3.0 && score < 3.51 = "Sangat Memuaskan"
+    | score >= 2.75 && score < 3.00 = "Memuaskan"
+    | score < 2.75 && score >= 0.0 = "Perlu Perbaikan"
+    | otherwise = ""
+
+-- tampilMahasiswa: mahasiswa -> String
+-- {tampilMahasiswa(M) mengubah Mahasiswa M menjadi string deskriptif}
+-- Dengan format:
+-- "Nama: <nama>, NIM: <nim>, IPK: <ipk>"
+tampilMahasiswa :: Mahasiswa -> String
+tampilMahasiswa (Mahasiswa name att score) = "Nama: " ++ name ++ ", " ++ "NIM: " ++ att ++ ", " ++ "IPK: " ++ show score  
+-- ++ Show float to string
